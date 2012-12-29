64114e24686c1375e59ff5b33ab4ce390c44b4c3
CC-Dog <cc941201@gmail.com>
2012-12-30 02:59:18 +0800

    Add restart button

--- src/ui/Info.java
+++ src/ui/Info.java
@@ -1,11 +1,15 @@
 package ui;
 
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
-public class Info extends JFrame {
+public class Info extends JFrame implements ActionListener {
 	public static JLabel label = new JLabel();
+	private Main frame;
 
 	public Info(Main frame) {
 		super("状态");
@@ -14,17 +18,42 @@ public class Info extends JFrame {
 		updateLabel(frame);
 		label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		getContentPane().add(label, BorderLayout.CENTER);
+
+		this.frame = frame;
+		JButton restartButton = new JButton("重新开始");
+		restartButton.addActionListener(this);
+		getContentPane().add(restartButton, BorderLayout.NORTH);
 	}
 
 	public static void updateLabel(Main frame) {
-		String side;
-		if (frame.board.turn)
-			side = "黑";
-		else
-			side = "白";
-		label.setText("<html><b>等待" + side + "方</b><br><br><br><b>黑方：</b>"
-				+ frame.board.blackNum + "<br><b>白方：</b>"
-				+ frame.board.whiteNum + "<br><br><b>空余：</b>"
-				+ frame.board.emptyNum + "</html>");
+		String side, line;
+		if (frame.finished) {
+			if (frame.winner == 1)
+				line = "黑方赢了";
+			else if (frame.winner == -1)
+				line = "白方赢了";
+			else
+				line = "平局";
+		} else if (frame.moving)
+			line = "请稍候...";
+		else {
+			if (frame.board.turn)
+				side = "黑";
+			else
+				side = "白";
+			line = "等待" + side + "方...";
+		}
+		label.setText("<html><b>" + line + "</b><br><br><br><b>黑方：</b>"
+				+ frame.board.blackNum + "个<br><b>白方：</b>"
+				+ frame.board.whiteNum + "个<br><br><b>空余：</b>"
+				+ frame.board.emptyNum + "个<br><br></html>");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.dispose();
+		frame.dispose();
+		frame = new Main();
+		frame.setVisible(true);
 	}
 }
