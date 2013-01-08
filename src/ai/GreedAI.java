19ec098aeb2cbd4d977edb595e4ccabcc0fe3a23
chenzhi <cz908640443@gmail.com>
2013-01-08 13:45:31 +0800

    Add random select function in GreedAI.

--- src/ai/GreedAI.java
+++ src/ai/GreedAI.java
@@ -1,6 +1,7 @@
 package ai;
 
 import ui.Coordinate;
+import java.util.*;
 
 public class GreedAI implements AI {
 	@Override
@@ -10,6 +11,16 @@ public class GreedAI implements AI {
 			for (int j = 0; j < 8; j++) {
 				Coordinate[] pieces = Determine.judge(new Coordinate(i, j),
 						yours, enemys);
+				if (pieces.length == 0)
+					continue;
+				if (pieces.length == max) {
+					Random rand = new Random();
+					int tmp = rand.nextInt(2);
+					if(tmp%2 == 0){
+						maxx = i;
+						maxy = j;
+					}
+				}
 				if (pieces.length > max) {
 					max = pieces.length;
 					maxx = i;
